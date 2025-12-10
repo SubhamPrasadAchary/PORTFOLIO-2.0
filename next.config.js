6f4c8ff211233218d43c094538b503b98d23e845
Subham Prasad Achary <acharysubham2803@gmail.com>
2025-12-10 13:25:10 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const path = require('path');
+
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['images.unsplash.com', 'source.unsplash.com'],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname),
+      '@/components': path.resolve(__dirname, 'components'),
+      '@/config': path.resolve(__dirname, 'config'),
+      '@/app': path.resolve(__dirname, 'app')
+    };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
